49ce0385c7fac7914d558f7a6107185341d78571
yy <eevgeniy@gmail.com>
2022-10-05 22:05:29 +0300

    Add find max picture implementation

---
+++ src/main/java/com/bobocode/largestnasapic/rest/NasaController.java
@@ -0,0 +1,27 @@
+package com.bobocode.largestnasapic.rest;
+
+import com.bobocode.largestnasapic.service.NasaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/mars/pictures/largest")
+public class NasaController {
+
+  private final NasaService nasaService;
+
+  @GetMapping(value = "/{sol}", produces = MediaType.IMAGE_PNG_VALUE)
+  public ResponseEntity<byte[]> getLargestPicture(@PathVariable Integer sol,
+      @RequestParam(required = false) Optional<String> camera) {
+    return ResponseEntity.ok(nasaService.getLargestPhoto(sol, camera).orElseThrow());
+  }
+}
---
+++ src/main/java/com/bobocode/largestnasapic/service/NasaService.java
@@ -0,0 +1,89 @@
+package com.bobocode.largestnasapic.service;
+
+import com.bobocode.largestnasapic.dto.Photo;
+import com.bobocode.largestnasapic.dto.PhotoWrapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpClient.Redirect;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.util.AbstractMap;
+import java.util.AbstractMap.SimpleImmutableEntry;
+import java.util.Map.Entry;
+import java.util.Optional;
+
+import static java.net.http.HttpResponse.BodyHandlers.ofByteArray;
+import static java.net.http.HttpResponse.BodyHandlers.ofString;
+
+@Service
+@Slf4j
+public class NasaService {
+
+  @Value(value = "${nasa.url}")
+  private String NASA_URL;
+
+  private final ObjectMapper objectMapper = new ObjectMapper();
+
+  @SneakyThrows
+  @Cacheable("max_pic")
+  public Optional<byte[]> getLargestPhoto(int sol, Optional<String> camera) {
+    var request = createRequest(sol, camera);
+    var httpClient = HttpClient.newBuilder().followRedirects(Redirect.ALWAYS).build();
+    log.info("Sending request to: {}", request.uri().toString());
+    var response = httpClient.send(request, ofString());
+    var photoWrapper = objectMapper.readValue(response.body(), PhotoWrapper.class);
+    SimpleImmutableEntry<String, Long> res = findMaxPictureUrlToSize(httpClient, photoWrapper);
+    log.info("Sending request to: {}", res.getKey());
+    var maxPicture = httpClient.send(createRequest(URI.create(res.getKey())), ofByteArray());
+    return Optional.of(maxPicture.body());
+  }
+
+  private SimpleImmutableEntry<String, Long> findMaxPictureUrlToSize(HttpClient httpClient, PhotoWrapper photoWrapper) {
+    return photoWrapper.getPhotos()
+        .parallelStream()
+        .map(Photo::getImgSrc)
+        .map(img -> getImgToLength(httpClient, img))
+        .max(Entry.comparingByValue())
+        .orElseThrow();
+  }
+
+  private AbstractMap.SimpleImmutableEntry<String, Long> getImgToLength(HttpClient httpClient, String img) {
+    try {
+      var request = HttpRequest.newBuilder()
+          .uri(URI.create(img))
+          .method(HttpMethod.HEAD.name(), BodyPublishers.noBody())
+          .build();
+      log.debug("Sending HEAD request to {}", request.uri().toString());
+      var headResponse = httpClient.send(request, ofString());
+      var length = headResponse.headers().firstValueAsLong(HttpHeaders.CONTENT_LENGTH).orElse(0);
+      return new AbstractMap.SimpleImmutableEntry<>(img, length);
+    } catch (IOException | InterruptedException e) {
+      e.printStackTrace();
+    }
+    return new AbstractMap.SimpleImmutableEntry<>("noContent", 0L);
+  }
+
+  private HttpRequest createRequest(int sol, Optional<String> camera) {
+    var uri = UriComponentsBuilder.fromHttpUrl(NASA_URL)
+        .queryParam("sol", sol)
+        .queryParamIfPresent("camera", camera)
+        .build().toUri();
+
+    return createRequest(uri);
+  }
+
+  private HttpRequest createRequest(URI uri) {
+    return HttpRequest.newBuilder(uri).GET().build();
+  }
+}
