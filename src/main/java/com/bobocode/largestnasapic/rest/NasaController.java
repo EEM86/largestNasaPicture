4f9c62e4b33d8025cc1faed37fa46c234e0ea614
yy <eevgeniy@gmail.com>
2022-10-19 21:23:27 +0300

    implement redis cache using docker instances

--- src/main/java/com/bobocode/largestnasapic/rest/NasaController.java
+++ src/main/java/com/bobocode/largestnasapic/rest/NasaController.java
@@ -1,27 +1,24 @@
 package com.bobocode.largestnasapic.rest;
 
-import com.bobocode.largestnasapic.service.NasaService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/mars/pictures/largest")
+@Slf4j
 public class NasaController {
 
-  private final NasaService nasaService;
+  private final NasaFeignClient nasaFeignClient;
 
-  @GetMapping(value = "/{sol}", produces = MediaType.IMAGE_PNG_VALUE)
-  public ResponseEntity<byte[]> getLargestPicture(@PathVariable Integer sol,
-      @RequestParam(required = false) Optional<String> camera) {
-    return ResponseEntity.ok(nasaService.getLargestPhoto(sol, camera).orElseThrow());
+  @GetMapping(produces = MediaType.IMAGE_PNG_VALUE)
+  public byte[] getLargestPictureProxy(@RequestParam Integer sol,
+      @RequestParam(required = false) String camera) {
+    return nasaFeignClient.getLargest(sol, camera);
   }
 }
